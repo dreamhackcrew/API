7e6db7e3d0c7cd955dd8282a6cf4373d82b696e2
stamp <stamp@stamp.se>
2013-04-28 22:58:23 +0200

    Added basic functionality to the food service

--- api/1/restricted/food.php
+++ api/1/restricted/food.php
@@ -3,7 +3,7 @@
 class food extends service {
 
     function _list($fid = null){
-
+        // Select event/events
         if(!isset($_GET['event'])){
             $events = db()->fetchSingle("SELECT id,name,start,end,active FROM events WHERE active ='Y' AND end >= CURRENT_DATE() ORDER BY start LIMIT 1");
             $event = $events['id']; 
@@ -11,8 +11,10 @@ class food extends service {
         else
             $event = $_GET['event'];
 
+        // Find all the meals
         $data = db()->fetchAll("SELECT * FROM user_food LEFT JOIN user_food_selected USING(fid) WHERE user_food.event=%d ",$event);
 
+        // Rearrange the struct and group it by meal
         $ret = array();
         if($data)
             foreach($data as $line){
@@ -24,15 +26,19 @@ class food extends service {
                     $ret[$line['fid']]['registered'][] = (int) $line['uid'];
             }
 
+        // Send the data
         return $ret;
-
     }
 
     function _checkin($uid,$fid){
-        $data = db()->fetchSingle("SELECT * FROM user_food_selected WHERE uid=%d AND fid=%d",$uid,$fid);
-        if(!$data){
+        // Check that the user have access to check in peope
+        $this->requireFlag('matincheckning');
+
+        // Find the meal
+        if ( !$data = db()->fetchSingle("SELECT * FROM user_food_selected WHERE uid=%d AND fid=%d",$uid,$fid) )
             trigger_error('Selected user not found on selected meal');
-        }
+
+        // Check in the user to the selected meal
         $tmp =  db()->query('UPDATE user_food_selected SET eaten=1 WHERE uid=%d AND fid=%d',$uid,$fid);
         return db()->affectedRows();
     }
